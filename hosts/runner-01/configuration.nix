325dd33c951fba1d1397d6ed8a42a9f7f6b79d2a
Dawid Ciężarkiewicz <dpc@dpc.pw>
2024-04-24 00:42:15 -0700

    chore: workaround some test issues

--- hosts/runner-01/configuration.nix
+++ hosts/runner-01/configuration.nix
@@ -4,6 +4,7 @@
   imports = [
     ./hardware-configuration.nix
     ./disk-config.nix
+    ./github-runner.nix
   ];
   boot.loader.grub = {
     # no need to set devices, disko will add all devices that have a EF02 partition to the list already
@@ -22,14 +23,14 @@
     pkgs.cachix
   ];
 
+  system.stateVersion = "23.11";
+
+  networking.enableIPv6 = true;
+
   users.users.root.openssh.authorizedKeys.keys = [
     # dpc
     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDRa93v8pzO+EXEH73odhh80VjkLVzPCaRw4K0sObdE9mbZqFB6k791Jm1cVQzHA+sCR4bnyOvA563ExLSGArw4IRxCZvZICSb8RI4QaIhCgf0NtwndKaBxnS2aWrJ/VKNmlZ4OsHMxrFtDRg0AHXBkj0H2O06bJ0+fiwiKdun1tqqi78qQPZkjaJoB227ipx3T0f9Oflj09iWVT3C0saaAiCtpa50ggjImom1FAwNF0gLhPGbSgUzsHzAndwexXWD5StAfWuePaapbQ0IIAY9ahlTKCXGSV0oS/IrBDjOfIaXoyzzgT4/xTz6dwie2g255mGTDn6k0CYkWX19H8xzT2TQ7e4ikNrXVdcRRRy4rd22MA75546RVD2mm36C0DnaUsnBUwymuQ02z33iTm8U7CZXQWpiKjwgqCtvs9zrsRx1YECHCw5ehUDt2nMw4ino42jthxV9bgQDQg/On7frBUXeKkd7L0UVfC71DW9AQQTvdHA2POpPhtoi7BznOeFMoVXxBMgJSgwGTH3ErY0zbvMLJNNROXby4rABmb7XTl5bav5DYD2lWzhcseN6a+/PgREyzllQxJqWQVQvA00JFuaNFLI7JeyIULUgyYuS5n/jEvmKKnzhwuGlHnIKF5UPViaF3WRiFSTop6taZNptBFWGBsG7eT8rTxb/FKtylVw== cardno:20_514_157"
   ];
-
-  system.stateVersion = "23.11";
-
-  networking.enableIPv6 = true;
 
 
   networking = {
@@ -37,6 +38,7 @@
       allowPing = true;
     };
   };
+
 
 
   # General server stuff
@@ -64,43 +66,4 @@
     };
   };
   services.journald.extraConfig = "SystemMaxUse=1G";
-
-
-  age.secrets = {
-    github-runner-token = {
-      file = ../../secrets/github-runner.age;
-      path = "/run/secrets/github-runner/nixos.token";
-      owner = "github-runner";
-      group = "github-runner";
-      mode = "600";
-    };
-  };
-
-  users.groups.github-runner = { };
-  users.users.github-runner = {
-    # behaves as normal user, needs a shell and home
-    isNormalUser = true;
-    group = "github-runner";
-  };
-
-  services.github-runners = lib.listToAttrs (map
-    (name: {
-      inherit name;
-      value = {
-        enable = true;
-        inherit name;
-        url = "https://github.com/fedimint";
-        tokenFile = "/run/secrets/github-runner/nixos.token";
-        user = "github-runner";
-      };
-    }) [
-    "runner-01-a"
-    "runner-01-b"
-    "runner-01-c"
-    "runner-01-d"
-    "runner-01-e"
-    "runner-01-f"
-    "runner-01-g"
-    "runner-01-h"
-  ]);
 }
---
+++ hosts/runner-01/github-runner.nix
@@ -0,0 +1,59 @@
+{ lib, pkgs, inputs, ... }:
+
+let
+  runnersNames = [
+    "runner-01-aa"
+    "runner-01-bb"
+    "runner-01-cc"
+    "runner-01-dd"
+    "runner-01-ee"
+    "runner-01-ff"
+  ];
+in
+{
+
+  age.secrets = {
+    github-runner-token = {
+      file = ../../secrets/github-runner.age;
+      path = "/run/secrets/github-runner/token";
+      # Note: this doesn't need to be readable to the runner user(s)
+      # Seems like NixOS script will register with GH first, then
+      # allow runner user to see only the post-registration creds,
+      # which is much safer.
+      owner = "root";
+      group = "root";
+      mode = "600";
+    };
+  };
+
+  users.groups.github-runner = { };
+  # ideally, we would just want one user, but due to
+  # https://github.com/btcsuite/btcd/pull/2177
+  # we need to tie each worker and it's home to it's workdir 1:1
+  users.users = lib.listToAttrs (map
+    (name: {
+      inherit name;
+      value = {
+        # behaves as normal user, needs a shell and home
+        isNormalUser = true;
+        group = "github-runner";
+        home = "/var/lib/github-runner/${name}/";
+      };
+    })
+    runnersNames);
+
+
+  services.github-runners = lib.listToAttrs (map
+    (name: {
+      inherit name;
+      value = {
+        enable = true;
+        inherit name;
+        url = "https://github.com/fedimint";
+        tokenFile = "/run/secrets/github-runner/token";
+        user = name;
+      };
+    })
+    runnersNames);
+
+}
