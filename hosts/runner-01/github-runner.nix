f4ca8b39b266f11f8c4ba6a57ccb533a4343b3bf
Dawid Ciężarkiewicz <dpc@dpc.pw>
2024-04-24 16:28:06 -0700

    chore(ci): fix docker access

--- hosts/runner-01/github-runner.nix
+++ hosts/runner-01/github-runner.nix
@@ -41,10 +41,12 @@ in
         isNormalUser = true;
         group = "github-runner";
         home = "/var/lib/github-runner/${name}/";
+        extraGroups = [ "docker" ];
       };
     })
     runnersNames);
 
+  virtualisation.docker.enable = true;
 
   services.github-runners = lib.listToAttrs (map
     (name: {
@@ -58,11 +60,14 @@ in
         serviceOverrides = {
           # we set home to the work dir, so mounting it RO messes things up
           ProtectHome = false;
+          # To access /var/run/docker.sock we need to be part of docker group,
+          # but it doesn't seem to work when it's mapped as `nobody` due to `PrivateUsers=true`
+          PrivateUsers = false;
         };
         extraPackages = [
-          pkgs.docker
           pkgs.cachix
           pkgs.gawk
+          pkgs.docker
         ];
       };
     })
