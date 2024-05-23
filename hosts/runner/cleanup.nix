96f340d354fe1442a87c36c5ed5391e1038904c6
Dawid Ciężarkiewicz <dpc@dpc.pw>
2024-05-23 14:57:57 -0700

    chore: clean up tmp files faster

--- hosts/runner/cleanup.nix
+++ hosts/runner/cleanup.nix
@@ -1,16 +1,27 @@
-{ config, lib, pkgs, ... }:
+{ lib, pkgs, ... }:
 let
   timerName = "cleanup-tmp";
 in
 {
-  systemd.services.${timerName} = {
-    description = "Clean up /tmp files not accessed in the last 24h";
-    serviceConfig = {
-      Type = "oneshot";
-      ExecStart = "${pkgs.findutils}/bin/find /tmp -type f -atime +1 -delete";
-      User = "root";
+  systemd.services.${timerName} =
+    let
+      script = pkgs.writeShellScript "cleanup-github-runner-tmp" ''
+        # all files created by the github-runner user
+        ${pkgs.findutils}/bin/find /tmp/ -mindepth 1 -type f -user github-runner -mmin +120 -delete
+        ${pkgs.findutils}/bin/find /tmp/ -mindepth 1 -type d -user github-runner -mmin +120 -empty -delete
+        # all files created by the nixbldX users (nixbld group)
+        ${pkgs.findutils}/bin/find /tmp/ -mindepth 1 -type f -group nixbld -mmin +120 -delete
+        ${pkgs.findutils}/bin/find /tmp/ -mindepth 1 -type d -group nixbld -mmin +120 -empty -delete
+      '';
+    in
+    {
+      description = "Clean up /tmp files not accessed in the last 24h";
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart = script;
+        User = "root";
+      };
     };
-  };
 
   systemd.timers.${timerName } = {
     description = "Timer for cleaning up /tmp files";
