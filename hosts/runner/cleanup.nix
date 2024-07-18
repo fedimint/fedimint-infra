52969ec2b330f9ffcea5b871a8141c7aecadc0d0
Dawid Ciężarkiewicz <dpc@dpc.pw>
2024-07-17 17:43:53 -0700

    chore: clean up /tmp more aggressively

--- hosts/runner/cleanup.nix
+++ hosts/runner/cleanup.nix
@@ -7,9 +7,9 @@ in
     let
       script = pkgs.writeShellScript "cleanup-github-runner-tmp" ''
         # tmp stuff  created by the github-runner user
-        ${pkgs.findutils}/bin/find /tmp/ -mindepth 1 -maxdepth 1 -user github-runner -mmin +120 -print0 | xargs -n 1 -0 rm -rf
+        ${pkgs.findutils}/bin/find /tmp/ -mindepth 1 -maxdepth 1 -user github-runner -mmin +60 -print0 | xargs -n 1 -0 rm -rf
         # tmp stuff created by the nixbldX users
-        ${pkgs.findutils}/bin/find /tmp/ -mindepth 1 -maxdepth 1 -group nixbld -mmin +120 -print0 | xargs -n 1 -0 rm -rf
+        ${pkgs.findutils}/bin/find /tmp/ -mindepth 1 -maxdepth 1 -group nixbld -mmin +60 -print0 | xargs -n 1 -0 rm -rf
       '';
     in
     {
