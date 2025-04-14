c9bce25baea59ff95426eceffbdef11298b6620a
Brad Stachurski <bradley.stachurski@gmail.com>
2025-04-14 08:00:05 -0600

    chore: bump fedimint version to v0.6.2

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
     };
 
     fedimint = {
-      url = "github:fedimint/fedimint?ref=v0.6.1-rc.0";
+      url = "github:fedimint/fedimint?ref=v0.6.2";
       # inputs.nixpkgs.follows = "nixpkgs";
     };
     fedimint-ui = {
