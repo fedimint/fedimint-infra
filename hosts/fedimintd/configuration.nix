1bb83d8e56881a1b13a4e3cc4fb3b282061aa12f
elsirion <elsirion@protonmail.com>
2025-06-05 16:27:46 +0200

    chore: remove automatic-timezoned

--- hosts/fedimintd/configuration.nix
+++ hosts/fedimintd/configuration.nix
@@ -43,7 +43,6 @@
 
   # General server stuff
   boot.tmp.cleanOnBoot = true;
-  services.automatic-timezoned.enable = true;
   nix = {
     # 2.21 seems to break crane vendoring crates
     # package = pkgs.nixVersions.nix_2_21;
