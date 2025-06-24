f189f0ada233e94ae26c3819e6802da97a99c4cf
Dawid Ciężarkiewicz <dpc@dpc.pw>
2025-06-24 14:03:27 -0700

    chore: try out custom iroh dns and relay

--- hosts/fedimintd/fedimintd.nix
+++ hosts/fedimintd/fedimintd.nix
@@ -97,10 +97,12 @@ in
     package = pkgs.fedimintd;
 
     environment = {
-      "RUST_LOG" = "fm=trace,info";
+      "RUST_LOG" = "fm=debug,info";
       "RUST_BACKTRACE" = "1";
       "FM_BIND_METRICS_API" = "[::1]:8275";
-      "FM_FORCE_IROH" = "1";
+      "FM_ENABLE_IROH" = "true";
+      "FM_IROH_DNS" = "https://dns.irohdns-eu-01.dev.fedimint.org";
+      "FM_IROH_RELAY" = "https://irohrelay-eu-01.dev.fedimint.org";
     };
 
     p2p = {
